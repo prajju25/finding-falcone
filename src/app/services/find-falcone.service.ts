50267debbca33fb41d871b36c4feb6eb81968eed
prajju25 <67220873+prajju25@users.noreply.github.com>
2020-08-13 02:10:03 +0530

    Update find-falcone.service.ts

--- src/app/services/find-falcone.service.ts
+++ src/app/services/find-falcone.service.ts
@@ -7,16 +7,16 @@ import { HttpClient } from '@angular/common/http'
 export class FindFalconeService {
   
   findFalcone(request: Object) {
-    return this.http.post('http://findfalcone.herokuapp.com/find', request).toPromise();
+    return this.http.post('https://findfalcone.herokuapp.com/find', request).toPromise();
   }
   getVehicles(): any {
-    return this.http.get('http://findfalcone.herokuapp.com/vehicles').toPromise();
+    return this.http.get('https://findfalcone.herokuapp.com/vehicles').toPromise();
   }
   getPlanets(): any {
-    return this.http.get('http://findfalcone.herokuapp.com/planets').toPromise();
+    return this.http.get('https://findfalcone.herokuapp.com/planets').toPromise();
   }
   async getToken(): Promise<Object> {
-    return this.http.post('http://findfalcone.herokuapp.com/token', null).toPromise();
+    return this.http.post('https://findfalcone.herokuapp.com/token', null).toPromise();
   }
   constructor(private http: HttpClient) { }
 }
